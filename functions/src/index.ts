1d9de60f02c0f691e776153ba6fbc74c717935c5
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-04-19 22:56:15 -0600

    Add pubsub handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions';
+import { defineString } from 'firebase-functions/params';
 import * as admin from 'firebase-admin';
+import { PubSub } from '@google-cloud/pubsub';
+import { Storage } from '@google-cloud/storage';
+import axios, { AxiosResponse } from 'axios';
+import { Stream } from 'stream';
 
 admin.initializeApp();
+
 const firestore = admin.firestore();
 const jobs = firestore.collection('jobs');
+
+const generationEndpoint = defineString('GENERATION_ENDPOINT');
+const storageBucket = defineString('STORAGE_BUCKET');
 
 export const generate = functions.https.onCall(async data => {
 	const prompt = data.prompt;
@@ -14,5 +23,37 @@ export const generate = functions.https.onCall(async data => {
 		prompt,
 	});
 
+	await new PubSub().topic('generation').publishMessage({
+		json: { prompt, jobId: job.id },
+	});
+
 	return { jobId: job.id };
 });
+
+export const generatePubSub = functions.pubsub
+	.topic('generation')
+	.onPublish(async message => {
+		const { prompt, jobId } = message.json;
+
+		try {
+			const job = jobs.doc(jobId);
+			job.update({ status: 'started' });
+
+			const response: AxiosResponse<Stream> = await axios({
+				url: generationEndpoint.value() + `?prompt=${encodeURIComponent(prompt)}`,
+				method: 'POST',
+				responseType: 'stream',
+			});
+
+			const storage = new Storage()
+				.bucket(storageBucket.value())
+				.file(jobId + '.schematic');
+
+			const stream = response.data.pipe(storage.createWriteStream());
+			stream.on('error', console.error);
+			stream.on('finish', () => job.update({ status: 'finished' }));
+		} catch (err) {
+			console.error(err);
+			jobs.doc(jobId).update({ status: 'failed' });
+		}
+	});
