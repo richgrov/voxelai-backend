9dfe70618013e618f36ac306f8fc5102e03c8a0d
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-05-03 15:14:36 -0600

    Authenticate Cloud Run requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,9 @@ import { defineString } from 'firebase-functions/params';
 import * as admin from 'firebase-admin';
 import { PubSub } from '@google-cloud/pubsub';
 import { Storage } from '@google-cloud/storage';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { Stream } from 'stream';
+import { GoogleAuth } from 'google-auth-library';
 
 admin.initializeApp();
 
@@ -13,6 +14,8 @@ const jobs = firestore.collection('jobs');
 
 const generationEndpoint = defineString('GENERATION_ENDPOINT');
 const storageBucket = defineString('STORAGE_BUCKET');
+
+const auth = new GoogleAuth();
 
 export const generate = functions.https.onCall(async data => {
 	const prompt = data?.prompt;
@@ -44,23 +47,37 @@ export const generatePubSub = functions.pubsub
 			const job = jobs.doc(jobId);
 			job.update({ status: 'started' });
 
-			const response: AxiosResponse<Stream> = await axios({
-				url: generationEndpoint.value() + `?prompt=${encodeURIComponent(prompt)}`,
-				method: 'POST',
-				responseType: 'stream',
-			});
-
-			const storage = new Storage()
+			const fileStream = await generateAndGetStream(prompt);
+			const storageStream = new Storage()
 				.bucket(storageBucket.value())
-				.file(jobId + '.schematic');
+				.file(jobId + '.schematic')
+				.createWriteStream();
 
-			const stream = response.data.pipe(storage.createWriteStream());
-			stream.on('finish', () => job.update({ status: 'finished' }));
-			stream.on('error', e => {
+			const pipe = fileStream.pipe(storageStream);
+			pipe.on('finish', () => job.update({ status: 'finished' }));
+			pipe.on('error', e => {
 				throw e;
 			});
 		} catch (err) {
-			jobs.doc(jobId).update({ status: 'failed' });
+			await jobs.doc(jobId).update({ status: 'failed' });
 			throw err;
 		}
 	});
+
+/**
+ * Makes a request to the configured generation endpoint and returns the response stream. If
+ * Running in production, the request will be authenticated with IAM.
+ */
+async function generateAndGetStream(prompt: string): Promise<Stream> {
+	const url = new URL(`/generate?prompt=${encodeURIComponent(prompt)}`, generationEndpoint.value());
+
+	if (process.env.FUNCTIONS_EMULATOR === 'true') {
+		return axios({ url: url.href, method: 'POST', responseType: 'stream'})
+			.then(response => response.data);
+	} else {
+		const client = await auth.getIdTokenClient(generationEndpoint.value());
+
+		return client.request({ url: url.href, method: 'POST', responseType: 'stream'})
+			.then(response => response.data as Stream);
+	}
+}
